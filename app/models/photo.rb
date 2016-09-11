d27b1cfd46bf112b812b5db06463594f4b8c6e06
Bobbie <obobiec@gmail.com>
2016-09-10 19:41:07 -0500

    aws setup region

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,17 @@
 class Photo < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, 
+										styles: { medium: "300x300>", thumb: "100x100>" }, 
+										default_url: "/images/:style/missing.png",
+										:storage => :s3,  
+										:s3_host_name => 's3-us-east-1.amazonaws.com',
+										:s3_credentials => { 
+											:bucket => ENV['S3_BUCKET_NAME'],
+											:access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+											:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+											:s3_region => 'us-east-1',
+											:url => 's3_domain_url'
+										}
+
+
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = false,
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -39,4 +39,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_host_name => 's3-us-east-1.amazonaws.com',
+    :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_region => 'us-east-1',
+    :url => 's3_domain_url'
+    }
+  }
 end
